772b1eeee1e2349c4b83d128e768ad53951e0df7
pervin0527 <zfbtldnz@gmail.com>
2024-03-24 11:51:27 +0900

    Updated 2024.03.24

--- coco.py
+++ coco.py
@@ -11,6 +11,7 @@ def _count_visible_keypoints(anno):
 def _has_only_empty_bbox(anno):
     return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
 
+
 def has_valid_annotation(anno):
     # if it's empty, there is no annotation
     if len(anno) == 0:
